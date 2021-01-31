e81a67d824d6c1f6ecbc9c650b04b56afb915331
TheoRacherR <theo.racher-raulin@outlook.com>
2021-01-31 17:17:13 +0100

    commit

--- app/src/drizzleOptions.js
+++ app/src/drizzleOptions.js
@@ -1,17 +1,13 @@
 import Web3 from "web3";
-import ComplexStorage from "./contracts/ComplexStorage.json";
 import ApplyProblem from './contracts/ApplyProblem.json';
-import AnswerProblem from './contracts/AnswerProblem.json';
 
 const options = {
   web3: {
     block: false,
     customProvider: new Web3("ws://localhost:8545"),
   },
-  contracts: [ ComplexStorage, ApplyProblem, AnswerProblem ],
-  events: {
-    ApplyProblem: ["Log"]
-  }
+  contracts: [ ApplyProblem ],
+  
 };
 
 export default options;
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,6 @@
 const ApplyProblem = artifacts.require("ApplyProblem");
-const ComplexStorage = artifacts.require("ComplexStorage");
 
 module.exports = function(deployer) {
   deployer.deploy(ApplyProblem);
-  deployer.deploy(ComplexStorage);
   
 };
